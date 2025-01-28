b37b29345a508404c6ccd5e5da2d6e3cbe470c52
혜원 <81088222+JSHWJ@users.noreply.github.com>
2025-01-28 21:33:26 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.04 ms, Memory: 83.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int price) {
+        if(500000 <= price){
+            return (int) (price * 0.8);
+        }else if(300000 <= price){
+            return (int) (price * 0.9);
+        }else if(100000 <= price){
+            return (int) (price * 0.95);
+        }
+        
+        return price;
+    }
+}
