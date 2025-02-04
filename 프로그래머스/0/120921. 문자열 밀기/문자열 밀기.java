112358745877caebc44ae6bf0a5b7fbe55b1072d
혜원 <81088222+JSHWJ@users.noreply.github.com>
2025-02-05 00:47:40 +0900

    [level 0] Title: 문자열 밀기, Time: 0.07 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String A, String B) {
+        char[] A_array = A.toCharArray();
+        char[] B_array = B.toCharArray();
+        
+        StringBuilder s = new StringBuilder();
+        
+        for(int i = 0; i < B.length(); i++){
+            if(B_array[i] == A_array[0]){
+                if(i == 0){
+                    if(A.equals(B)){
+                        return 0;
+                    }
+                    continue;
+                }
+                
+                s.append(B.substring(i, B.length()));
+                s.append(B.substring(0, i));
+                
+                if(s.toString().equals(A)){
+                    return i;
+                    
+                }else s.setLength(0);
+            }
+        }
+        
+        
+        return -1;
+    }
+}
