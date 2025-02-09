56cab29036d005ff661c61b6735e2482e59744b7
혜원 <81088222+JSHWJ@users.noreply.github.com>
2025-02-09 23:49:02 +0900

    [level 0] Title: 옹알이 (1), Time: 0.88 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,32 +1,19 @@
+
 class Solution {
     public int solution(String[] babbling) {
-        
-        String[] words = {"aya", "ye", "woo", "ma" };
         int answer = 0;
-        
-        for(String b : babbling){
-            if(isEmpty(b, words)){
-                answer++;
+
+        for(int i =0; i < babbling.length; i++) {
+            babbling[i] = babbling[i].replace("aya", "1");
+            babbling[i] = babbling[i].replace("woo", "1");
+            babbling[i] = babbling[i].replace("ye", "1");
+            babbling[i] = babbling[i].replace("ma", "1");
+            babbling[i] = babbling[i].replace("1", "");
+            if(babbling[i].isEmpty()) {
+                answer = answer + 1;
             }
         }
-        
+
         return answer;
     }
-    
-    private boolean isEmpty(String b, String[] words){
-        
-        for(String word : words){
-            if(b.indexOf(word) != -1){
-                if(b.indexOf(word, b.indexOf(word) + word.length()) != -1){
-                    return false;
-                }
-                b = b.replace(word, " ");
-            }
-        }
-        
-        return b.trim().isEmpty(); //  trim()을 해야하는 이유 : yayae 인 경우 aya를 ""로 치환해버리면 ye가 남게 되는데 이게 카운트 되어서 오류가 남
-        
-    }
-    
-    
 }
