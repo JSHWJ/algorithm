441649c3aa830f1d613a1f0e2d11babc52c025ae
혜원 <81088222+JSHWJ@users.noreply.github.com>
2025-03-07 00:19:01 +0900

    [level 2] Title: 타겟 넘버, Time: 6.70 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,48 +1,22 @@
-// import java.util.*;
-
-// class Solution {
-//     public int solution(int[] numbers, int target) {
-//         Queue<int[]> queue = new LinkedList<>();
-//         queue.offer(new int[]{0, 0}); 
-//         int count = 0;
-        
-        
-//         while(!queue.isEmpty()){
-//             int[] temp = queue.poll();
-//             int sum = temp[0];
-//             int index = temp[1];
-            
-//             if(index == numbers.length){
-//                 if (target == sum) {
-//                     count++;
-//                 }
-//                 continue;
-//             }
-//             queue.offer(new int[]{sum + numbers[index], index + 1});
-//             queue.offer(new int[]{sum - numbers[index], index + 1});
-//         }
-        
-//         return count;
-//     }
-// }
-
 class Solution {
     int count = 0;
-    
+
     public int solution(int[] numbers, int target){
         dfs(numbers, 0, 0, target);
         return count;
     }
     
     public void dfs(int[] numbers, int index, int sum, int target){
-            if(index == numbers.length){
-                if(target == sum){
-                    count++;
-                }
-                return;
+        if(index == numbers.length){
+            if(target == sum){
+                count++;
             }
+            return;
+        }
         
-            dfs(numbers, index + 1, sum + numbers[index], target);
-            dfs(numbers, index + 1, sum - numbers[index], target);
+        dfs(numbers, index + 1, sum + numbers[index], target);
+        dfs(numbers, index + 1, sum - numbers[index], target);
     }
 }
+
+// index를 기준으로 한 단계씩 깊이를 내려가면서 먼저 한 선택지를 다 탐색하고 다음 선택지를 검사한다 즉 dfs
