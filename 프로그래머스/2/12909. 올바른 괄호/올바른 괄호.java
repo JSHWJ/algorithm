0b3341450dee9dc6555c0345169b8b5835ecd964
혜원 <81088222+JSHWJ@users.noreply.github.com>
2025-03-21 00:29:43 +0900

    [level 2] Title: 올바른 괄호, Time: 22.09 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,23 +3,23 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         Stack<Character> stack = new Stack<>();
+        
         if(s.length() % 2 != 0){
             return false;
         }
         
-        for(char c : s.toCharArray()){
-            if(c == '('){
-                stack.push(c);
-            }else{
-                if(stack.isEmpty()){
-                    return false;
-                }
-                stack.pop();
+        for(char i : s.toCharArray()){
+            if(i == '('){ 
+                stack.push(i);
+            } else { // ) 이면
+                if(!stack.isEmpty()){
+                    stack.pop(); // 스택에서 (을 꺼냄
+                } else return false; // empty 상태이면 짝이 맞지 않음
             }
         }
         
-        if(stack.isEmpty()){
-            return true;
-        }else return false;
+        if(stack.isEmpty()){ // stack에 아무것도 없으면
+            return true;  //  true임
+        } else return false;
     }
 }
