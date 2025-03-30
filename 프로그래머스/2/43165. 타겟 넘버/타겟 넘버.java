df73173ce0972a895bb62e0138625028ac5bf9d7
혜원 <81088222+JSHWJ@users.noreply.github.com>
2025-03-31 00:09:15 +0900

    [level 2] Title: 타겟 넘버, Time: 5.88 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,22 +1,22 @@
 class Solution {
-    int count = 0;
-
+    int answer = 0;
+    
     public int solution(int[] numbers, int target){
-        dfs(numbers, 0, 0, target);
-        return count;
+        dfs(numbers, target, 0, 0);
+        return answer;
     }
     
-    public void dfs(int[] numbers, int index, int sum, int target){
-        if(index == numbers.length){
+    public void dfs(int[] numbers, int target, int depth, int sum){
+        if(depth == numbers.length){
             if(target == sum){
-                count++;
+              answer++;  
             }
             return;
         }
-        
-        dfs(numbers, index + 1, sum + numbers[index], target);
-        dfs(numbers, index + 1, sum - numbers[index], target);
+        dfs(numbers, target, depth + 1, sum - numbers[depth]);
+        dfs(numbers, target, depth + 1, sum + numbers[depth]);
     }
+
 }
 
-// index를 기준으로 한 단계씩 깊이를 내려가면서 먼저 한 선택지를 다 탐색하고 다음 선택지를 검사한다 즉 dfs
+// 이건 방법을 하나하나 다 해야하니까 dfs 같음
