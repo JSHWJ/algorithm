9ef0858d7ac987588c039bd8cbd0ab17ef7249a3
혜원 <81088222+JSHWJ@users.noreply.github.com>
2025-04-05 21:58:32 +0900

    [level 1] Title: K번째수, Time: 2.35 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,14 +1,20 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
+        List<Integer> list = Arrays.stream(array)
+            .boxed()
+            .collect(Collectors.toList());
+        
         int[] answer = new int[commands.length];
         
         for(int i = 0; i < commands.length; i++){
-            int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
-            Arrays.sort(temp);
-            answer[i] = temp[commands[i][2] - 1];
+            List<Integer> temp = new ArrayList<>(list.subList(commands[i][0]-1, commands[i][1]));
+            Collections.sort(temp);
+            answer[i] = (int) temp.get(commands[i][2]-1);
         }
+
         
         return answer;
     }
