343b342c638e991135ea5c61f0eea39316ddd6b2
혜원 <81088222+JSHWJ@users.noreply.github.com>
2025-05-03 20:34:10 +0900

    [level 1] Title: 명예의 전당 (1), Time: 3.58 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < score.length; i++){
+            if(list.size() < k){
+                list.add(score[i]);
+                Collections.sort(list);
+                answer[i] = list.get(0);
+            } else{
+                if(list.get(0) >= score[i]){
+                    answer[i] = list.get(0);
+                } else{
+                    list.remove(0);
+                    list.add(score[i]);
+                    Collections.sort(list);
+                    answer[i] = list.get(0);
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
