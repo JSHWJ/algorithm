ce2a83f70065c61452eae3a9a40c3e40ce6d1023
혜원 <81088222+JSHWJ@users.noreply.github.com>
2025-05-04 20:57:19 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.40 ms, Memory: 55.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -3,10 +3,10 @@ import java.util.*;
 class Solution {
     public int solution(int[][] maps) {
         int width = maps[0].length;
-        int height = maps.length;
+        int height = maps.length; // 세로
         
-        int[] dx = {0, 0, 1, -1}; // x좌표 우, 좌, 하, 상
-        int[] dy = {1, -1, 0, 0}; // y좌표 우, 좌, 하, 상
+        int[] dx = {1, 0, -1, 0 };// 우, 하, 좌, 상
+        int[] dy = {0, 1, 0, -1}; // 우, 하, 좌, 상
         
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{0, 0, 1}); // x좌표, y좌표, 이동한 거리
@@ -16,11 +16,11 @@ class Solution {
         
         while(!queue.isEmpty()){
             int[] current = queue.poll();
-            int x = current[0];
-            int y = current[1];
+            int x = current[0]; // x좌표
+            int y = current[1]; // y 좌표
             int distance = current[2];
             
-            if(x == (height - 1) && y == (width - 1)){
+            if(x == (width - 1) && y == (height - 1)){
                 return distance;
             }
             
@@ -28,9 +28,9 @@ class Solution {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 
-                if(nx >= 0 && nx < height && ny >= 0 && ny < width && maps[nx][ny] != 0 && !visited[nx][ny]){
-                    queue.offer(new int[] {nx, ny, distance + 1});
-                    visited[nx][ny] = true;
+                if(nx >= 0 && nx < width && ny >= 0 && ny < height && maps[ny][nx] != 0 && !visited[ny][nx]){
+                    queue.offer(new int[]{nx, ny, distance + 1});
+                    visited[ny][nx] = true;
                 }
             }
         }
