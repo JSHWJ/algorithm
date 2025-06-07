5b86e8aeb78cff023316f2b5603bba67e5e88d85
혜원 <81088222+JSHWJ@users.noreply.github.com>
2025-06-07 23:33:26 +0900

    [level 2] Title: 타겟 넘버, Time: 7.33 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -2,21 +2,23 @@ class Solution {
     int answer = 0;
     
     public int solution(int[] numbers, int target){
-        dfs(numbers, target, 0, 0);
+        dfs(numbers, target, 0 ,0);
         return answer;
     }
     
     public void dfs(int[] numbers, int target, int depth, int sum){
-        if(depth == numbers.length){
+        if(numbers.length == depth){
             if(target == sum){
-              answer++;  
-            }
+                answer++;
+            } 
             return;
         }
-        dfs(numbers, target, depth + 1, sum - numbers[depth]);
+        
         dfs(numbers, target, depth + 1, sum + numbers[depth]);
+        dfs(numbers, target, depth + 1, sum - numbers[depth]);
     }
+
 
 }
 
-// 이건 방법을 하나하나 다 해야하니까 dfs 같음
+// dfs : 깊이 우선 탐색 
