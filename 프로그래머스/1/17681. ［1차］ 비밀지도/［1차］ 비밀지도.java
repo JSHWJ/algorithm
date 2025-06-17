fb9972b1e0bb90fda7ef6fbd5783451bdf41e023
혜원 <81088222+JSHWJ@users.noreply.github.com>
2025-06-18 01:23:21 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.07 ms, Memory: 69.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        
+        for(int i = 0; i < n; i++){
+            int merged = arr1[i] | arr2[i];
+            StringBuilder sb = new StringBuilder();
+            
+            for(int j = n - 1; j >= 0 ; j--){
+                sb.append(((merged >> j) & 1) == 1 ? '#' : ' ');
+            }
+            
+            answer[i] = sb.toString();
+        }
+        return answer;
+    }
+}
