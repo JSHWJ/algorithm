b368b81adffd25102ca68fa5d010eae2be5172e0
혜원 <81088222+JSHWJ@users.noreply.github.com>
2025-06-24 14:25:47 +0900

    [level 2] Title: 카펫, Time: 0.14 ms, Memory: 87.8 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,21 +2,25 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int brown, int yellow) {
-        List<int[]> divisors = new ArrayList<>(); // 가로, 세로
+        int number = brown - 4; // 모서리 개수 제외한 숫자
+        List<int[]> list = new ArrayList<>();
         
         for(int i = 1; i <= Math.sqrt(yellow); i++){
             if(yellow % i == 0){
-                int another = yellow / i;
-                divisors.add(new int[]{Math.max(another, i), Math.min(another, i)});
+                list.add(new int[]{Math.max(i, yellow/i), Math.min(i, yellow/i)});
             }
         }
         
-        for(int[] divisor : divisors){
-            if((brown - 4) == divisor[0] * 2 + divisor[1] * 2){
-                return new int[]{divisor[0] + 2, divisor[1] + 2};
+        for(int[] l : list){
+            int width = l[0];
+            int height = l[1];
+            
+            if((width * 2 + height * 2) == number){
+                return new int[]{width + 2, height + 2};
             }
         }
         
         return null;
+        
     }
 }
