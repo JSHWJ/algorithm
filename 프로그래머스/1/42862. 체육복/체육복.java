d857141bc6bd55d4454f0c1b1b825f88ade39ea5
혜원 <81088222+JSHWJ@users.noreply.github.com>
2025-06-26 18:53:07 +0900

    [level 1] Title: 체육복, Time: 0.36 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,40 +1,47 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        List<Integer> lostPeople = new ArrayList<>();
-        List<Integer> reservePeople = new ArrayList<>();
+        List<Integer> lostList = new ArrayList<>();
+        List<Integer> reserveList = new ArrayList<>();
         
         for(int l : lost){
-            lostPeople.add(l);
+            lostList.add(l);
         }
         
         for(int r : reserve){
-            reservePeople.add(r);
+            reserveList.add(r);
         }
         
-        Collections.sort(lostPeople);
-        Collections.sort(reservePeople);
+        Collections.sort(lostList);
+        Collections.sort(reserveList);
         
-        Iterator<Integer> iterator = reservePeople.iterator();
+        for(int r : reserve){
+            if(lostList.contains(r)){
+                lostList.remove(Integer.valueOf(r));
+                reserveList.remove(Integer.valueOf(r));
+            }
+        }
+        
+        Iterator<Integer> iterator = lostList.iterator();
+    
         while(iterator.hasNext()){
-            int p = iterator.next();
-            if(lostPeople.contains(p)){
-                iterator.remove(); 
-                lostPeople.remove(Integer.valueOf(p));
+            int num = iterator.next();
+            if(reserveList.contains(num - 1)){
+                iterator.remove();
+                reserveList.remove(Integer.valueOf(num - 1));
+            } else if(reserveList.contains(num + 1)){
+                iterator.remove();
+                reserveList.remove(Integer.valueOf(num + 1));
             }
         }
+        System.out.println(lostList);
+        return n - lostList.size();
         
-        
-        for(int p : reservePeople){
-            if(lostPeople.contains(p - 1)){
-                lostPeople.remove(Integer.valueOf(p - 1));
-            } else if(lostPeople.contains(p + 1)){
-                lostPeople.remove(Integer.valueOf(p + 1));
-            }
-        }
-        
-        return n - lostPeople.size();
     }
 }
+
+// 정렬을 하지 않으면, 앞의 순회된 도난 학생이 먼저 가져가게 되면, 뒤의 학생이 못 가져가게 된다.
+// 하지만 정렬을 하면 앞에서부터 번호가 작은 학생대로, 차례대로 가져가서 순회 순서로 인해 결과가 달라지는 경우를 방지할 수 있다.
+// 그리고 + 1 부터 확인하면, 뒤의 학생부터 빌리게 되어, 앞의 학생이 유일하게 빌릴 수 있는 경우를 놓치게 될 수 있다.
+// 그래서 번호가 작은 것부터 확인하면, 작은 번호부터 순서대로 빌려주는 구조가 된다. 
