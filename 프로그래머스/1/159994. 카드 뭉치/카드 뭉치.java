e6ec314a8cd598cde4078d8de4e4e728f28bd157
혜원 <81088222+JSHWJ@users.noreply.github.com>
2025-07-18 15:10:55 +0900

    [level 1] Title: 카드 뭉치, Time: 0.12 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -2,16 +2,27 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        List<String> card1_list = new ArrayList<>(Arrays.asList(cards1));
-        List<String> card2_list = new ArrayList<>(Arrays.asList(cards2));
+        Queue<String> queue_1 = new LinkedList<>();
+        Queue<String> queue_2 = new LinkedList<>();
         
-        for(String word : goal){
-            if(!card1_list.isEmpty() && card1_list.get(0).equals(word)){
-                card1_list.remove(0);
-            } else if(!card2_list.isEmpty() && card2_list.get(0).equals(word)){
-               card2_list.remove(0);
-            } else return "No";
+        for(String card1 : cards1){
+            queue_1.offer(card1);
         }
+        
+        for(String card2 : cards2){
+            queue_2.offer(card2);
+        }
+        
+        for(String g : goal){
+            if(!queue_1.isEmpty() && queue_1.peek().equals(g)){
+                queue_1.poll();
+            } else if(!queue_2.isEmpty() && queue_2.peek().equals(g)){
+                queue_2.poll();
+            } else {
+                return "No";
+            }
+        }
+        
         return "Yes";
     }
 }
